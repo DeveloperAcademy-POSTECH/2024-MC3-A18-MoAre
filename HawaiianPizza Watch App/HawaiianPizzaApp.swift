818341d706908b854e0d6e64c1742c8362e64ba0
leewanjae <dhkswo1@gmail.com>
2024-07-24 21:09:51 +0900

    project init

---
+++ HawaiianPizza Watch App/HawaiianPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HawaiianPizzaApp.swift
+//  HawaiianPizza Watch App
+//
+//  Created by LeeWanJae on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct HawaiianPizza_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HawaiianPizza/HawaiianPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HawaiianPizzaApp.swift
+//  HawaiianPizza
+//
+//  Created by LeeWanJae on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct HawaiianPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
