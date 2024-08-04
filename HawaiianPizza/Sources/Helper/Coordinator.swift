f7091725f82546a243caecd26ddbb56719b58c3f
wltnryu <yebbrnjs@gmail.com>
2024-08-05 01:58:17 +0900

    fix: TenSecView에서 TimerView로의 전환 오류 해결

--- HawaiianPizza/Sources/Helper/Coordinator.swift
+++ HawaiianPizza/Sources/Helper/Coordinator.swift
@@ -37,6 +37,7 @@ final class Coordinator: ObservableObject {
             RoutinePlanningView()
         case .timer:
             TimerView()
+            .navigationBarBackButtonHidden()
         case .complete:
             CompleteView()
         case .routineDetail:
